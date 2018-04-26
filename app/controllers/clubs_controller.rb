6b48fe1702de692e0fc993390ccb2f03e6f8a903
juandsierra82 <juandsierra82@gmail.com>
2018-04-26 13:54:57 -0400

    controller set up and tested

---
+++ app/controllers/clubs_controller.rb
@@ -0,0 +1,6 @@
+class ClubsController < ApplicationController
+  def index
+    @clubs = Club.all
+    render json: @clubs
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+club = Club.find_or_create_by({
+    name: 'Club Sport'
+})
